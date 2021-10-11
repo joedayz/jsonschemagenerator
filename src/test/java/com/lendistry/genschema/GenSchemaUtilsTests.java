6a8f845ac1af7a782b5818b9df91235438d36470
Jose Diaz <jose.diaz@joedayz.pe>
2021-10-10 23:20:50 -0500

    test merge different property types

--- src/test/java/com/lendistry/genschema/GenSchemaUtilsTests.java
+++ src/test/java/com/lendistry/genschema/GenSchemaUtilsTests.java
@@ -17,4 +17,16 @@ public class GenSchemaUtilsTests {
         assertTrue(p3.getExamples().contains("dog"));
         assertTrue(p3.getExamples().contains("foobar"));
     }
+
+    @Test
+    public void differentTypesPropertyMergeTest(){
+        Property p1 = new Property("#/p1","string","foobar");
+        Property p2 = new Property("#/p1","integer",22);
+        Property p3 = GenSchemaUtils.merge(p1,p2);
+        assertEquals("#/p1",p3.getId());
+        assertTrue(p3.typeEquals("string"));
+        assertTrue(p3.typeEquals("integer"));
+        assertTrue(p3.getExamples().contains("foobar"));
+        assertTrue(p3.getExamples().contains(22));
+    }
 }
