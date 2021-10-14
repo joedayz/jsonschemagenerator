79e6307a90bafc2b991b4a31a6b69f9e858861a5
Jose Diaz <jose.diaz@joedayz.pe>
2021-10-14 17:55:30 -0500

    refactor code in the process of JsonSchemaGenerator

--- src/main/java/pe/joedayz/jsonschemagenerator/JsonSchemaGenerator.java
+++ src/main/java/pe/joedayz/jsonschemagenerator/JsonSchemaGenerator.java
@@ -2,7 +2,6 @@ package pe.joedayz.jsonschemagenerator;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.io.FileUtils;
@@ -71,87 +70,20 @@ public class JsonSchemaGenerator {
         JsonNode node = null;
         switch (nodeType) {
             case ARRAY:
+
                 if (key.equals("applicationFeatures")) {
 
                     Map<String, Object> properties = new HashMap<>();
                     LinkedList<IfThen> ifThenStack = new LinkedList<>();
 
-
-                    for (int i = 0; i < jsonNode.get(key).size(); i++) {
-
-                        IfThen ifThen = new IfThen();
-                        if (i == 0) {
-                            result.append("array\", \"items\": { \"properties\":");
-                        }
-
-
-                        for (Iterator<String> iterator = jsonNode.get(key).get(i).fieldNames(); iterator.hasNext(); ) {
-
-                            String fieldName = iterator.next();
-
-
-                            if (properties.get(fieldName) == null) {
-                                JsonNode field = jsonNode.get(key).get(i).get(fieldName);
-
-
-                                properties.put(fieldName, field);
-
-                                if (fieldName.equals("dataType")) {
-                                    ifThen.getIfProperty().setDataType(field);
-                                }
-                                if (fieldName.equals("itemType")) {
-                                    ifThen.getIfProperty().setItemType(field);
-                                }
-                                if (fieldName.equals("name")) {
-                                    ifThen.getIfProperty().setName(field);
-                                }
-
-                                if (fieldName.equals("format")) {
-                                    ifThen.getThenProperty().setFormat(field);
-                                }
-
-                                if (fieldName.equals("value")) {
-                                    ifThen.getThenProperty().setValue(field);
-                                }
-
-                            } else {
-
-                                JsonNode field = jsonNode.get(key).get(i).get(fieldName);
-                                if (fieldName.equals("dataType")) {
-                                    ifThen.getIfProperty().setDataType(field);
-                                }
-                                if (fieldName.equals("itemType")) {
-                                    ifThen.getIfProperty().setItemType(field);
-                                }
-                                if (fieldName.equals("name")) {
-                                    ifThen.getIfProperty().setName(field);
-                                }
-
-                                if (fieldName.equals("format")) {
-                                    ifThen.getThenProperty().setFormat(field);
-                                }
-
-                                if (fieldName.equals("value")) {
-                                    ifThen.getThenProperty().setValue(field);
-                                }
-                            }
-
-
-                        }
-
-                        ifThenStack.addLast(ifThen);
-                    }
+                    builtPropertiesForApplicationFeatures(jsonNode, key, result, properties, ifThenStack);
 
                     ObjectNode objectNode = objectMapper.createObjectNode();
 
                     properties.forEach((k, v) -> {
-                        if (k.equals("format") || k.equals("value")) {
-
-
-                        } else {
+                        if (!(k.equals("format") || k.equals("value"))) {
                             objectNode.set(k, (JsonNode) v);
                         }
-
                     });
 
                     result.append(outputAsString(null, null, objectNode.toString(), JsonNodeType.ARRAY));
@@ -163,55 +95,8 @@ public class JsonSchemaGenerator {
 
                         IfThen ifThen = it;
 
-                        result.append("{");
-                        result.append("\"if\" : { \"properties\": {");
-
-                        String constValue = ifThen.getIfProperty().getDataType().toString();
-                        result.append("\"dataType\" : {");
-                        result.append("\"const\" : ").append(constValue).append("");
-                        result.append("},");
-
-                        constValue = ifThen.getIfProperty().getItemType().toString();
-                        result.append("\"itemType\" : {");
-                        result.append("\"const\" : ").append(constValue).append("");
-                        result.append("},");
-
-                        constValue = ifThen.getIfProperty().getName().toString();
-                        result.append("\"name\" : {");
-                        result.append("\"const\" : ").append(constValue).append("");
-                        result.append("}");
-
-                        result.append("}},");
-
-                        result.append("\"then\" : { \"properties\": {");
-
-                        result.append("\"format\" : { \"type\": \"object\", \"properties\": ");
-
-                        JsonNode formatThen = ifThen.getThenProperty().getFormat();
-
-                        try {
-                            result.append(outputAsString(null, null, formatThen.toString(), JsonNodeType.ARRAY));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        result.append("}, ");
-
-                        result.append("\"value\" : { \"type\": \"object\", \"properties\": ");
-
-
-                        JsonNode valueThen = ifThen.getThenProperty().getValue();
-
-                        try {
-                            result.append(outputAsString(null, null, valueThen.toString(), JsonNodeType.ARRAY));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        result.append("}}}  }," );
+                        builtIfThenBlockForApplicationFeatures(result, ifThen);
                     });
-
-
 
                     int charInd = result.lastIndexOf(",");
                     if(charInd!=-1) result.deleteCharAt(charInd);
@@ -247,6 +132,110 @@ public class JsonSchemaGenerator {
         }
 
         return result.toString();
+    }
+
+    private static void builtPropertiesForApplicationFeatures(JsonNode jsonNode, String key, StringBuilder result, Map<String, Object> properties, LinkedList<IfThen> ifThenStack) {
+        for (int i = 0; i < jsonNode.get(key).size(); i++) {
+
+            IfThen ifThen = new IfThen();
+
+            if (i == 0) {
+                result.append("array\", \"items\": { \"properties\":");
+            }
+
+
+            for (Iterator<String> iterator = jsonNode.get(key).get(i).fieldNames(); iterator.hasNext(); ) {
+
+                String fieldName = iterator.next();
+
+
+                if (properties.get(fieldName) == null) {
+                    JsonNode field = jsonNode.get(key).get(i).get(fieldName);
+                    properties.put(fieldName, field);
+
+                    generateIfThenAccordingDataTypeItemTypeAndName(ifThen, fieldName, field);
+
+                } else {
+
+                    JsonNode field = jsonNode.get(key).get(i).get(fieldName);
+
+                    generateIfThenAccordingDataTypeItemTypeAndName(ifThen, fieldName, field);
+                }
+
+
+            }
+
+            ifThenStack.addLast(ifThen);
+        }
+    }
+
+    private static void builtIfThenBlockForApplicationFeatures(StringBuilder result, IfThen ifThen) {
+        result.append("{");
+        result.append("\"if\" : { \"properties\": {");
+
+        String constValue = ifThen.getIfProperty().getDataType().toString();
+        result.append("\"dataType\" : {");
+        result.append("\"const\" : ").append(constValue).append("");
+        result.append("},");
+
+        constValue = ifThen.getIfProperty().getItemType().toString();
+        result.append("\"itemType\" : {");
+        result.append("\"const\" : ").append(constValue).append("");
+        result.append("},");
+
+        constValue = ifThen.getIfProperty().getName().toString();
+        result.append("\"name\" : {");
+        result.append("\"const\" : ").append(constValue).append("");
+        result.append("}");
+
+        result.append("}},");
+
+        result.append("\"then\" : { \"properties\": {");
+
+        result.append("\"format\" : { \"type\": \"object\", \"properties\": ");
+
+        JsonNode formatThen = ifThen.getThenProperty().getFormat();
+
+        try {
+            result.append(outputAsString(null, null, formatThen.toString(), JsonNodeType.ARRAY));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        result.append("}, ");
+
+        result.append("\"value\" : { \"type\": \"object\", \"properties\": ");
+
+
+        JsonNode valueThen = ifThen.getThenProperty().getValue();
+
+        try {
+            result.append(outputAsString(null, null, valueThen.toString(), JsonNodeType.ARRAY));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        result.append("}}}  }," );
+    }
+
+    private static void generateIfThenAccordingDataTypeItemTypeAndName(IfThen ifThen, String fieldName, JsonNode field) {
+        if (fieldName.equals("dataType")) {
+            ifThen.getIfProperty().setDataType(field);
+        }
+        if (fieldName.equals("itemType")) {
+            ifThen.getIfProperty().setItemType(field);
+        }
+        if (fieldName.equals("name")) {
+            ifThen.getIfProperty().setName(field);
+        }
+
+        if (fieldName.equals("format")) {
+            ifThen.getThenProperty().setFormat(field);
+        }
+
+        if (fieldName.equals("value")) {
+            ifThen.getThenProperty().setValue(field);
+        }
     }
 
     private static String cleanup(String dirty) {
